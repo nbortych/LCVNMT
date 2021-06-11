84e3a9730736788fc6336f1aa5523f35cd11dd7a
Nikita <nikita.bortych@gmail.com>
2021-06-11 16:10:36 +0200

    init stats in init too

--- NMT/joeynmt/joeynmt/training.py
+++ NMT/joeynmt/joeynmt/training.py
@@ -268,15 +268,14 @@ class TrainManager:
             # see https://nvidia.github.io/apex/amp.html#opt-levels
 
         # initialize training statistics
-        if not self.ddp:
-            self.stats = self.TrainStatistics(
-                steps=0,
-                stop=False,
-                total_tokens=0,
-                best_ckpt_iter=0,
-                best_ckpt_score=np.inf if self.minimize_metric else -np.inf,
-                minimize_metric=self.minimize_metric,
-                early_stopping_patience=self.early_stopping_patience)
+        self.stats = self.TrainStatistics(
+            steps=0,
+            stop=False,
+            total_tokens=0,
+            best_ckpt_iter=0,
+            best_ckpt_score=np.inf if self.minimize_metric else -np.inf,
+            minimize_metric=self.minimize_metric,
+            early_stopping_patience=self.early_stopping_patience)
 
         # model parameters
         if "load_model" in train_config.keys() and not self.ddp:
