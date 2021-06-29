9efc0d00b08d895972411eee8db38a5285095cc8
Nikita <nikita.bortych@gmail.com>
2021-06-29 10:52:46 +0200

    debug memory utility

--- NMT/joeynmt/joeynmt/helpers.py
+++ NMT/joeynmt/joeynmt/helpers.py
@@ -363,3 +363,14 @@ def latest_checkpoint_update(target: pathlib.Path,
         return current_last
     link.symlink_to(target)
     return None
+
+
+def debug_memory():
+    import collections, gc, resource, torch
+    print('maxrss = {}'.format(
+        resource.getrusage(resource.RUSAGE_SELF).ru_maxrss), flush=True)
+    tensors = collections.Counter((str(o.device), o.dtype, tuple(o.shape))
+                                  for o in gc.get_objects()
+                                  if torch.is_tensor(o))
+    for line in tensors.items():
+        print('{}\t{}'.format(*line), flush=True)
